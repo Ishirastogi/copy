2e8052f398c0fef6c3be7568f79b7b84f41f455b
Ishi Rastogi <ishi.rastogi@icloud.com>
2025-12-22 19:14:34 +0530

    removed

--- components/lib/firebase.js
+++ components/lib/firebase.js
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDSX0w1Rp_KU2SyNq-k-2u1l3glSGVR-u8",
-  authDomain: "oodwallah.firebaseapp.com",
-  projectId: "oodwallah",
-  appId: "1:69311026194:web:701bb4c37f0194acf1f4cb",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDSX0w1Rp_KU2SyNq-k-2u1l3glSGVR-u8",
+//   authDomain: "oodwallah.firebaseapp.com",
+//   projectId: "oodwallah",
+//   appId: "1:69311026194:web:701bb4c37f0194acf1f4cb",
+// };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+// export const auth = getAuth(app);
+// export const googleProvider = new GoogleAuthProvider();
